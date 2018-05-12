76d815ed500a5ae5f4a49811e37225fc504dfa12
Duriseti <duriseti@amazon.com>
2018-05-11 23:32:04 -0500

    Initial commit

---
+++ src/main/java/com/dg/cookbook/App.java
@@ -0,0 +1,13 @@
+package com.dg.cookbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
